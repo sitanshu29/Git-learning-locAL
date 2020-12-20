788bf51f32ad36cee128fa7e7a5915c01a215da9
sitanshu <sitanshushukla29@gmail.com>
2020-12-20 22:31:01 +0530

    Understanding conflicts

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-function onclick()
-{
-    document.getElementById("heading1").style.color = "blue";
-}
-||||||| 5854a28
-=======
 function onclick()
 {
     document.getElementById("heading1").style.color = "blue";
 }
 
 window.alert("Learning Branching");
->>>>>>> sidebar
